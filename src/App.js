49e0a06cd60c44913a3622b8618e649470e8fb5a
Rob Dawson <rdawson164@gmail.com>
2024-12-12 23:11:46 +0000

    added routing and styling for active nav buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
 import Container from 'react-bootstrap/Container';
+import {Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Sign In</h1>
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="/signin" element={<h1>Sign In</h1>} />
+          <Route exact path="/signup" element={<h1>Sign Up</h1>} />
+          <Route element={<p>Page not Found!</p>} />
+        </Routes>
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from '../styles/NavBar.module.css'
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
       <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
+        <NavLink to="/">
           <Navbar.Brand>
             Recipie Mate
           </Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>Sign in</Nav.Link>
-              <Nav.Link>Sign up</Nav.Link>
+            <NavLink to="/" className={({ isActive }) => isActive ? `${styles.NavLink} ${styles.Active}` : styles.NavLink}>Home</NavLink>
+            <NavLink to="/signin" className={({ isActive }) => isActive ? `${styles.NavLink} ${styles.Active}` : styles.NavLink}>Sign In</NavLink>
+            <NavLink to="/signup" className={({ isActive }) => isActive ? `${styles.NavLink} ${styles.Active}` : styles.NavLink}>Sign Up</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
