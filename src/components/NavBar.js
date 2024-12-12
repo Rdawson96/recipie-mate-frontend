b90a50f74fb681eeaf542587e96e7e1f3a8a4643
Rob Dawson <rdawson164@gmail.com>
2024-12-12 22:31:40 +0000

    styles pages and started routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
-    return <div>
-        Navigation bar
-    </div>
-};
+    return (
+      <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Container>
+          <Navbar.Brand>
+            Recipie Mate
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Sign in</Nav.Link>
+              <Nav.Link>Sign up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  };
 
 export default NavBar;
 
