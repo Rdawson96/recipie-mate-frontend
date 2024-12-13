b75eedb1e1d377436f24df260d7cea59c663b73d
Rob Dawson <rdawson164@gmail.com>
2024-12-13 01:10:30 +0000

    added form to sign up form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar';
 import styles from './App.module.css';
 import Container from 'react-bootstrap/Container';
 import {Route,Routes} from 'react-router-dom';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route exact path="/" element={<h1>Home Page</h1>} />
           <Route exact path="/recipies" element={<h1>Recipies</h1>} />
           <Route exact path="/signin" element={<h1>Sign In</h1>} />
-          <Route exact path="/signup" element={<h1>Sign Up</h1>} />
+          <Route exact path="/signup" element={<SignUpForm />} />
           <Route element={<p>Page not Found!</p>} />
         </Routes>
       </Container>
