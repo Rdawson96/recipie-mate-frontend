71bc27c5df94721ef86ec5de41ca252d588757e9
Rob Dawson <rdawson164@gmail.com>
2024-12-13 01:31:26 +0000

    added sign in forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styles from './App.module.css';
 import Container from 'react-bootstrap/Container';
 import {Route,Routes} from 'react-router-dom';
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<h1>Home Page</h1>} />
           <Route exact path="/recipies" element={<h1>Recipies</h1>} />
-          <Route exact path="/signin" element={<h1>Sign In</h1>} />
+          <Route exact path="/signin" element={<SignInForm />} />
           <Route exact path="/signup" element={<SignUpForm />} />
           <Route element={<p>Page not Found!</p>} />
         </Routes>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -7,42 +7,30 @@ import appStyles from "../../App.module.css";
 
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
 
-const SignUpForm = () => {
+const SignInForm = () => {
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-1 p-md-1">
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Sign Up</h1>
+          <h1 className={styles.Header}>Sign In</h1>
         </Container>
     <Form>
       <Form.Group className="mb-3" controlId="Username">
         <Form.Label className="d-none">Username</Form.Label>
         <Form.Control type="text" placeholder="Username" name="Username"/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label className="d-none">Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
       <Form.Group className="mb-3" controlId="Password">
         <Form.Label className="d-none">Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="confirmPassword">
-        <Form.Label className="d-none">Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm Password" />
-      </Form.Group>
+
       <Button type="submit">
         Submit
       </Button>
     </Form>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
       </Col>
     </Row>
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
